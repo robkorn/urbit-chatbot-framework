1e924c509c57838f15ff1620652d9a56025fcc16
Ben McCormick <benmcc100@gmail.com>
2021-02-07 20:11:55 -0600

    removed own ship name...

--- examples/poll-bot/src/main.rs
+++ examples/poll-bot/src/main.rs
@@ -151,6 +151,6 @@ fn respond_to_message(authored_message: AuthoredMessage) -> Option<Message> {
 }
 
 fn main() {
-    let chat_bot = Chatbot::new_with_local_config(respond_to_message, "~bacrys", "testchat-30");
+    let chat_bot = Chatbot::new_with_local_config(respond_to_message, "~ship-name", "chat-name");
     chat_bot.run();
 }
